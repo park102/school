7caa5460660ddc789d758ea09b96faf744b79198
park102 <BJohn316er@gmail.com>
2024-03-04 15:44:08 -0500

    spelling

--- If-Else Statements/school.java
+++ If-Else Statements/school.java
@@ -15,12 +15,12 @@ public static void main(String[] args) {
     int rand = ThreadLocalRandom.current().nextInt(min, max + 1);
     System.out.println("guess a number between " + min + " and " + max);
     
-    boolean ans = false;
-    while (!ans) {
-        Integer gess = scan.nextInt();
-            if (gess < rand) { System.out.println("number to small"); 
-            } else if (gess > rand) { System.out.println("number to big");
-            } else if (gess == rand) { ans = true; System.out.println("that is correct"); }
+    boolean answer = false;
+    while (!answer) {
+        Integer guess = scan.nextInt();
+            if (guess < rand) { System.out.println("number to small"); 
+            } else if (guess > rand) { System.out.println("number to big");
+            } else if (guess == rand) { answer = true; System.out.println("that is correct"); }
 
         }
     }
